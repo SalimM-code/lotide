47d9078dceee3beab87c069de129a403cd8af3e2
salimM-code <mustafasalim000@gmail.com>
2021-09-20 10:16:36 -0400

    made code more dry.

--- head.js
+++ head.js
@@ -1,15 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+const assertEqual = require('./assertEqual');
 
 const head = function(arry) {
     return arry[0];
 }
 
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"))
+
+module.exports = head;
---
+++ test/headTest.js
@@ -0,0 +1,6 @@
+// const assertEqual = require('../assertEqual');
+const head = require('../head',);
+const assertEqual = require('../assertEqual');
+
+console.log(assertEqual(head([5,6,7]), 6));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"))
