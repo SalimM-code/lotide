0126321785d52214975c8a8da22f394c4f339402
salimM-code <mustafasalim000@gmail.com>
2021-09-20 13:05:14 -0400

    added exports/require

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,12 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  let result;
-  if (arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index])) {
-  result = true;
-  } else {
-  result = false;
-  }
-  return result;
-  };
+const eqArrays = require('./eqArrays');
 
   const assertArrayEqual = function(arry1, arry2) {
     if (eqArrays(arry1, arry2)) {
@@ -16,4 +8,5 @@ const eqArrays = function(arr1, arr2) {
     }
   }
 
-  assertArrayEqual([1,2,3], [1,2,3]);
+  module.exports = assertArrayEqual;
+  
---
+++ test/assertArrayEqualTest.js
@@ -0,0 +1,4 @@
+const assertArrayEqual = require('../assertArrayEqual');
+const eqArrays = require('../eqArrays')
+
+assertArrayEqual([1,2,3], [1,2,3]);
